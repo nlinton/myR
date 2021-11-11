1dc289edac3cc3149c042da4991335ae31e77ba9
nlinton <nlinton.epi@gmail.com>
2021-11-11 20:36:21 +0900

    add date formatting function

--- R/formatting.R
+++ R/formatting.R
@@ -11,3 +11,9 @@ getmin <- \(x,digit) { plyr::round_any(min(x), digit, f=floor) }
 #' @param digit divisor number to be used
 #' @export
 getmax <- \(x,digit) { plyr::round_any(max(x), digit, f=ceiling) }
+
+#' Export date as yyyymmdd format
+#'
+#' @param date input is a date
+#' @export
+yyyymmdd <- \(date) paste0(sprintf("%04d-",year(ymd(date))),sprintf("%02d-",month(ymd(date))),(sprintf("%02d",day(ymd(date)))))

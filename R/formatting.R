dbe5e9c903044c50f900786866441081c5ad1708
nlinton <nlinton.epi@gmail.com>
2021-11-12 16:43:55 +0900

    minor function formatting edits

--- R/formatting.R
+++ R/formatting.R
@@ -3,16 +3,16 @@
 #' @param x input data
 #' @param digit divisor number to be used
 #' @export
-getmin <- \(x,digit) { plyr::round_any(min(x), digit, f=floor) }
+getmin <- \(x, digit) { plyr::round_any(min(x), digit, f=floor) }
 
 #' Round up using a given digit type
 #'
 #' @param x input data
 #' @param digit divisor number to be used
 #' @export
-getmax <- \(x,digit) { plyr::round_any(max(x), digit, f=ceiling) }
+getmax <- \(x, digit) { plyr::round_any(max(x), digit, f=ceiling) }
 
-#' Export date as yyyymmdd format
+#' Format date as character yyyymmdd
 #'
 #' @param date input is a date
 #' @export

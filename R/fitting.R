da0d561090f39a090cb5f15e43148b89b7b4bcc8
nlinton <nlinton.epi@gmail.com>
2021-11-12 16:00:55 +0900

    update function description

--- R/fitting.R
+++ R/fitting.R
@@ -1,7 +1,8 @@
 #' Fit reporting delay and get parameters
 #'
-#' @param dat dataframe that contains
-#' @param digit divisor number to be used
+#' @param dat dataframe that contains a variable called rd, which is the empirical reporting delay
+#' @param init_pars divisor number to be used (max length = 2)
+#' @param dist character string of distribution type to be used
 #' @export
 get_reporting_delay_pars <- \(dat, init_pars, dist) {
   rd <- dat$rd
